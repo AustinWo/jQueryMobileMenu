6f7411191e148c908731fac6ef5b891b30d458a0
Walan Worachet <wworachet@Walans-MacBook-Air-2.local>
2014-10-08 20:08:53 -0700

    plan out js steps

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Problem: doesnt look good in smaller browser width and small devices
+// SolutionL: Hide the text links and swap them out with a more appropriate navigation
+
+// Create a select and append to menu
+// Cycle over menu links
+  // Create an option
+  // option's value is the href of the link
+  // option's text is the text of the link
+  // append option to select
+
+// Create button
+// Blind click to button
+  // go to select's location
+
+// Modify CSS to hide links on small width and show button and select
+  // Also hides select and button on larger width
+// Deal with selected options depending on current page
